81164077645cec03816deb1bfbda8282509a5645
coreyoses <corey.oses@gmail.com>
2016-09-27 17:13:49 -0400

    changes to cs

--- js/dropdown_menu.js
+++ js/dropdown_menu.js
@@ -4,9 +4,35 @@ function performDropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+function closeDropDown() {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
+
+        /*var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }*/
+        closeDropDown();
+    }
+}
+
+/*window.onmousemove = function(event) {
+    if (!event.target.className('full-dropdown')) {
 
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
@@ -17,4 +43,4 @@ window.onclick = function(event) {
             }
         }
     }
-}
+}*/
